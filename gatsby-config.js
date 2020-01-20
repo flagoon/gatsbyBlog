66ccab968a9cb34c236752b5667fa86105295cbe
Paweł Kochanek <pawel.kochanek@gmail.com>
2020-01-20 20:10:06 +0100

    Handling images in MD

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,32 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: { name: `src`, path: `${__dirname}/src/` },
     },
+    // plugin for loading all files from path into graphql.
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: { name: `images`, path: `${__dirname}/src/` },
+    },
     // plugin for transforming Markdown files, multiple config files can be added for using images,
     // code highlight etc in Markdown. In graphql, allMarkdownRemark and markdownRemark are created
     // to search necessary fields.
-    'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     // plugin for using typescript with gatsby.
     'gatsby-plugin-typescript',
     // plugin for using styled-components in gatsby.
--- src/articles/first-testing-article/index.md
+++ src/articles/first-testing-article/index.md
@@ -2,8 +2,10 @@
 title: 'First'
 date: '2017-08-09'
 description: 'First testing article'
+featuredImage: yoda.jpg
 ---
 
-##Pandas are really sweet.
+## Pandas are really sweet.
 
+![Drag Racing](hacktoberfest.png)
 Here's a video of a panda eating sweets.
