c96c12875bc3baefda48ecaacc12b8c3d4b11985
Paweł Kochanek <pawel.kochanek@gmail.com>
2020-01-20 18:55:22 +0100

    Added comments for gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,20 @@ module.exports = {
     description: `My personal blog for the stuff I'm learning`,
   },
   plugins: [
-    'gatsby-plugin-typescript',
-    'gatsby-transformer-remark',
-    'gatsby-plugin-styled-components',
-    { resolve: 'gatsby-plugin-typography', pathToConfigModule: `src/utils/typography` },
+    // plugin for loading all files from path into graphql.
     {
       resolve: 'gatsby-source-filesystem',
       options: { name: `src`, path: `${__dirname}/src/` },
     },
+    // plugin for transforming Markdown files, multiple config files can be added for using images,
+    // code highlight etc in Markdown. In graphql, allMarkdownRemark and markdownRemark are created
+    // to search necessary fields.
+    'gatsby-transformer-remark',
+    // plugin for using typescript with gatsby.
+    'gatsby-plugin-typescript',
+    // plugin for using styled-components in gatsby.
+    'gatsby-plugin-styled-components',
+    // typography config.
+    { resolve: 'gatsby-plugin-typography', pathToConfigModule: `src/utils/typography` },
   ],
 };
